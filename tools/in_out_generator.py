343dc8806c0542a339ff3fe3314890555437be45
Luckysting <work.romanuel@gmail.com>
2020-04-28 14:52:24 +0300

    fix colors

--- tools/in_out_generator.py
+++ tools/in_out_generator.py
@@ -77,14 +77,19 @@ reader = pyvista.read('in_geometry.vti')
 shape = np.array([reader.dimensions[1], reader.dimensions[0]]) - 1
 material = reader['Material']
 in_img = material.reshape(shape)
-in_img = Image.fromarray(np.flip(in_img, axis=0))
+in_img = np.flip(in_img, axis=0)
+in_img = in_img + abs(np.min(in_img))
+in_img = in_img / np.max(in_img) * 255
+in_img = Image.fromarray(in_img)
 
 with open('in_file.in', 'w') as in_file:
     in_file.write(in_file_content.format(cylinders=cylinders).replace('#geometry_view', 'geometry_view'))
 
-api('in_file.in', 620, gpu=[0])
+api('in_file.in', 2)
 merge_files('in_file', removefiles=True)
 data, dt = get_output_data('in_file_merged.out', 1, 'Ez')
+data = data + abs(np.min(data))
+data = data / np.max(data) * 255
 out_img = Image.fromarray(data)
 out_img = out_img.resize(in_img.size)
 in_img.convert('RGB').save('in.png', 'PNG')
